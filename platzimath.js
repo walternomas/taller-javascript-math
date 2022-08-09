25230287cc7bc08afe6cf97760882d2534994211
walternomas <w.barriosvazquez@gmail.com>
2022-08-09 05:11:56 -0300

    moda finalizada

--- platzimath.js
+++ platzimath.js
@@ -3,6 +3,7 @@ function esPar(lista) {
 }
 
 function calcularMediana(lista) {
+  lista = ordenarLista(lista);
   const listaEsPar = esPar(lista);
   if(listaEsPar) {
     const indexMitadDerecha = lista.length / 2;
@@ -29,6 +30,37 @@ function calcularPromedio(lista) {
   //console.log(sumWithInitial);
 
   return sumWithInitial/lista.length;
+
+}
+
+function ordenarLista(listaDesordenada) {
+  const compare = (a, b) => a - b;
+  const listaOrdenada = listaDesordenada.sort(compare);
+  return listaOrdenada;
+}
+
+function ordenarListaBidimensional(listaDesordenada) {
+  const compare = (a, b) => b[1] - a[1];
+  const listaOrdenada = listaDesordenada.sort(compare);
+  return listaOrdenada;
+}
+
+function calcularModa(lista) {
+  const listaCount = {};
+  for (let i = 0; i < lista.length; i++) {
+    const elemento = lista[i];
+    (listaCount[elemento])
+      ? listaCount[elemento] += 1
+      : listaCount[elemento] = 1;
+  }
+  //console.log(listaCount);
+  const listaArray = Object.entries(listaCount);
+  const listaOrdenada = ordenarListaBidimensional(listaArray);
+  if(listaOrdenada[0][1] === listaOrdenada[1][1]) {
+    return 'No hay moda';
+  } else {
+    return listaOrdenada[0][0];
+  }
 
 }
 
